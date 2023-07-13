313284ba08521c334dca7fe03303779fc19a0d67
Adimas <admsfarhan@gmail.com>
2023-07-13 13:01:30 +0700

    ABOUT UPDATE

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
           <span className='text-red-400 text-neon-red'>n</span>
           <span className='text-blue-400 text-neon-blue'>&nbsp;exum</span>
           <p className="text-2xl pt-[50px] justify-center text-neon-blue">Hi! meet Nexum, your reliable chatting website.<br></br> this website allows you to messages,video calls,and also sharing files with <br></br>your friend, your beloved and
-          of course anyone in this world as long u know their ID!<br></br></p>
+          of course anyone in this world as long as u know their ID!<br></br></p>
           <p className="text-2xl pt-[50px] text-neon-green">Also, meet our developer!</p>
           <span class="flex justify-center space-x-[200px] pt-5">
        <a href='https://www.instagram.com/admsfrhn/' target='_blank'><span><img src={PX5}className="pt-5 rounded w-[350px] transition duration-300 ease-in-out hover:opacity-50 "></img><p className='text-2xl text-neon-red pt-5'>Adimas Farhan Putranto</p></span></a>
